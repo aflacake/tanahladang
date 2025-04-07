d4b3d72ef17a1ab6d393027fb20de1ce55a210ab
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-04-07 10:19:11 +0700

    Update alat.js
    Menyalakan dan mematikan suara latar belakang, musik

--- alat.js
+++ alat.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 // cek orientasi perangkat mengharuskan ke lanskap
 function cekOrientasi() {
-	if (window.innerWidth > window.innerHeight) {
-		document.querySelector('.pesanLanskap').style.display = 'none';
-		document.querySelector('.content').style.display = 'block';
-	} else {
-		// jika mode portait
-		document.querySelector('.pesanLanskap').style.display = 'block';
-		document.querySelector('.content').style.display = 'none';
-	}
+    if (window.innerWidth > window.innerHeight) {
+        document.querySelector('.pesanLanskap').style.display = 'none';
+        document.querySelector('.content').style.display = 'block';
+    } else {
+        // jika mode portait
+        document.querySelector('.pesanLanskap').style.display = 'block';
+        document.querySelector('.content').style.display = 'none';
+    }
 }
 cekOrientasi();
 window.addEventListener('resize', cekOrientasi);
@@ -25,9 +25,59 @@ document.getElementById("hariA").textContent = hariArray[hariHariIni];
 
 
 
+// fullscreen
+const fullscreenTombol = document.getElementById("fullScreenBtn");
+
+function sekarangmasukFullscreen() {
+    return document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
+}
+fullscreenTombol.addEventListener('click', function() {
+    if (sekarangmasukFullscreen()) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+        fullscreenTombol.textContent = "Masuk dari Mode Fullscreen";
+    } else {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+      }
+     fullscreenTombol.textContent = "Keluar dari Mode Fullscreen";
+ }
+});
+
+
+
 // reset data
 document.getElementById('resetGimBtn').addEventListener('click', function() {
-	localStorage.clear();
-	alert("Semua data gim telah dihapus, Anda tidak dapat memulihkannya");
-	window.location.reload();
+    localStorage.clear();
+    alert("Semua data gim telah dihapus, Anda tidak dapat memulihkannya");
+    window.location.reload();
+});
+
+
+
+// play musik
+const playMusikBtn = document.getElementById("playMusikBtn");
+const audio = document.getElementById("backgroundMusik");
+
+playMusikBtn.addEventListener('click', function() {
+	if (audio.paused) {
+		audio.play();	
+		playMusikBtn.textContent = "Matikan musik";
+	} else {
+		audio.pause();
+		playMusikBtn.textContent = "Nyalakan musik";
+	}
 });
