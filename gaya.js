232a275c203a83cf2aabda749f5bae1cbc091cf6
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-04-26 14:18:09 +0700

    Menambahkan gaya
    Gaya untuk JavaScript perintilan tag kode untuk komitmen ada di induknya

---
+++ gaya.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+const pesanLanskap = document.getElementById("pesanLanskap");
+pesanLanskap.style.display = "block";
+pesanLanskap.style.padding = '20px';
+pesanLanskap.style.margin = '20px';
+pesanLanskap.style.fontSize = '16px';
+
+
+// Rumah kambing, pohon, dan sumur
+const kotaRumahKambing = document.getElementById("kotaRumahKambing");
+kotaRumahKambing.style.zIndex = '9999';
+
+const kotaPohonBurung = document.getElementById("kotaPohonBurung");
+kotaPohonBurung.style.zIndex = '9999';
+
+const kontenSumur = document.getElementById("kontenSumur");
+kontenSumur.style.zIndex = '9999';
+});
